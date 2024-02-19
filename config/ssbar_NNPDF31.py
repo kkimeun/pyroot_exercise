ae3bd1b06a2a384fbd16b5e4d753d5a22e31bfc7
kkimeun <kkimeun22@snu.ac.kr>
2024-02-19 21:36:05 +0900

    fix histo filepath

--- config/ssbar_NNPDF31.py
+++ config/ssbar_NNPDF31.py
@@ -4,7 +4,8 @@ _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
     "name":"s_sbar with NNPDF31",
-    "input":_maindir+"/mg_output/histos_ssbar_NNPDF31.root",
+    #"input":_maindir+"/mg_output/histos_ssbar_NNPDF31.root",
+    "input":_maindir+"/mg_output/histos_ssbarwithpT.root",
     "hist":OrderedDict({
         "s":{"path":"log_x_s","name":"s","title":"s","color":4},
         "sbar":{"path":"log_x_sbar","name":"sbar","title":"#bar{s}","color":2},
