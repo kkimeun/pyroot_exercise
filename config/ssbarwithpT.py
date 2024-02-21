51b2e957ccfb099ca789fe7b6b9a00b851abef79
kkimeun <kkimeun22@snu.ac.kr>
2024-02-22 00:53:50 +0900

    to solve deno problem

--- config/ssbarwithpT.py
+++ config/ssbarwithpT.py
@@ -3,14 +3,14 @@ from collections import OrderedDict
 _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
-    "name":"electron pT using NNPDF23",
-    "input":_maindir+"/mg_output/histos_ssbar_NNPDF23.root",
+    "name":"electron pT > 200 using NNPDF31",
+    "input":_maindir+"/mg_output/histos_ssbarwithpT.root",
     "hist":OrderedDict({
-        "e":{"path":"pT_e","name":"e","title":"e","color":6},
+        #"e":{"path":"pT_e","name":"e","title":"e","color":6},
         "e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
         "e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
     }),
-    "title":"pp->l vl electron pT using NNPDF23_nlo_as_0119",
+    "title":"pp->l vl electron pT > 200 using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"pT(e)",
     "ytitle":"Event",
     "deno":"e, x<0.03",
