2d823acae05b4083db892997b935c0f63cfae4ff
kkimeun <kkimeun22@snu.ac.kr>
2024-02-22 01:49:51 +0900

    lepton eta distribution in range of x>0.03, x<0.03 -1

---
+++ config/ssbarwithEta.py
@@ -0,0 +1,18 @@
+import os
+from collections import OrderedDict
+_maindir = os.getenv("_maindir_pyroot_git")
+
+conf={
+    "name":"electron Eta using NNPDF31",
+    "input":_maindir+"/mg_output/histos_ssbarwithpTEta.root",
+    "hist":OrderedDict({
+        #"e":{"path":"pT_e","name":"e","title":"e","color":6},
+        "e, x>0.03":{"path":"Eta_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
+        "e, x<0.03":{"path":"Eta_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
+    }),
+    "title":"pp->l vl electron Eta using NNPDF23_nlo_as_0119",
+    "xtitle":"pT(e)",
+    "ytitle":"Event",
+    "deno":"e, x<0.03",
+
+}
