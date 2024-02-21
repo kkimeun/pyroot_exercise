733d6ae143092316270531ff53c9b950f8727535
kkimeun <kkimeun22@snu.ac.kr>
2024-02-21 12:39:54 +0900

    default lepton pT using NNPDF23

--- config/ssbar_NNPDF23.py
+++ config/ssbar_NNPDF23.py
@@ -3,14 +3,14 @@ from collections import OrderedDict
 _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
-    "name":"s_sbar with lepton pT > 200, using NNPDF23",
-    "input":_maindir+"/mg_output/histos_ssbarwithpT.root",
+    "name":"s_sbar using NNPDF23",
+    "input":_maindir+"/mg_output/histos_ssbar_NNPDF23.root",
     #"input":_maindir+"/mg_output/histos_ssbarwithpT.root",
     "hist":OrderedDict({
         "s":{"path":"log_x_s","name":"s","title":"s","color":4},
         "sbar":{"path":"log_x_sbar","name":"sbar","title":"#bar{s}","color":2},
     }),
-    "title":"pp->l vl with lepton pT > 200, using NNPDF23_nlo_as_0119",
+    "title":"pp->l vl using NNPDF23_nlo_as_0119",
     "xtitle":"log(x)",
     "ytitle":"Event",
     "deno":"s",
