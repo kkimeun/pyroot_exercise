3da84e120a533f7e6223fd3597dbeb7ca01add07
kkimeun <kkimeun22@snu.ac.kr>
2024-02-22 19:49:18 +0900

    change eta minimum -1, x standard 0.001

--- config/ssbarwithEta.py
+++ config/ssbarwithEta.py
@@ -7,12 +7,12 @@ conf={
     "input":_maindir+"/mg_output/histos_ssbarwithpTEta.root",
     "hist":OrderedDict({
         #"e":{"path":"pT_e","name":"e","title":"e","color":6},
-        "e, x>0.0003":{"path":"Eta_e_x>0.0003","name":"e, x>0.0003","title":"e, x>0.0003","color":2},
-        "e, x<0.0003":{"path":"Eta_e_x<0.0003","name":"e, x<0.0003","title":"e, x<0.0003","color":4},
+        "e, x>0.001":{"path":"Eta_e_x>0.001","name":"e, x>0.001","title":"e, x>0.001","color":2},
+        "e, x<0.001":{"path":"Eta_e_x<0.001","name":"e, x<0.001","title":"e, x<0.001","color":4},
     }),
     "title":"pp->l vl electron Eta < 0.5 using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"pT(e)",
     "ytitle":"Event",
-    "deno":"e, x<0.0003",
+    "deno":"e, x<0.001",
 
 }
