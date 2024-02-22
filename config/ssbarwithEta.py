ce7bb4764966a20aa8cb69fb56778654c94ce398
kkimeun <kkimeun22@snu.ac.kr>
2024-02-22 19:56:23 +0900

    redraw adjust range of eta -5 to 5, x standard 0.001

--- config/ssbarwithEta.py
+++ config/ssbarwithEta.py
@@ -3,14 +3,14 @@ from collections import OrderedDict
 _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
-    "name":"electron Eta < 0.5 using NNPDF31",
+    "name":"electron Eta < 1.5 using NNPDF31",
     "input":_maindir+"/mg_output/histos_ssbarwithpTEta.root",
     "hist":OrderedDict({
         #"e":{"path":"pT_e","name":"e","title":"e","color":6},
         "e, x>0.001":{"path":"Eta_e_x>0.001","name":"e, x>0.001","title":"e, x>0.001","color":2},
         "e, x<0.001":{"path":"Eta_e_x<0.001","name":"e, x<0.001","title":"e, x<0.001","color":4},
     }),
-    "title":"pp->l vl electron Eta < 0.5 using NNPDF31_nnlo_hessian_pdfas",
+    "title":"pp->l vl electron Eta < 1.5 using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"pT(e)",
     "ytitle":"Event",
     "deno":"e, x<0.001",
