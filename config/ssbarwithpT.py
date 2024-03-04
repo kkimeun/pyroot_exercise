27074d84e6b06c57432e84326b9de130297f713c
kkimeun <kkimeun22@snu.ac.kr>
2024-03-04 15:15:53 +0900

    change linecolor in ratioplot -2

--- config/ssbarwithpT.py
+++ config/ssbarwithpT.py
@@ -10,10 +10,10 @@ conf={
         "e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
         "e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
     }),
-    #"hratio":OrderedDict({
-	#"e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
-	#"e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
-    #}),
+    "hratio":OrderedDict({
+	"e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
+	"e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
+     }),
     "title":"pp->l vl electron pT using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"pT(e)",
     "ytitle":"Event",
