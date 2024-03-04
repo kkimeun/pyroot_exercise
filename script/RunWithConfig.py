8656f050d635a39289dee80e5a0206d4bc59fa5e
kkimeun <kkimeun22@snu.ac.kr>
2024-03-04 15:12:36 +0900

    change linecolor in ratioplot -1

--- script/RunWithConfig.py
+++ script/RunWithConfig.py
@@ -33,7 +33,14 @@ def Run(confpath,normtype=0):
     myplotter.DrawNoRatio()
     ##--RatioPlot
     myplotter.SetDeno(deno)
-    myplotter.DrawRatio()               
+    for p in conf["hratio"]:
+        p_path=conf["hratio"][p]["path"]
+	p_name=conf["hratio"][p]["name"]
+	p_title=conf["hratio"][p]["title"]
+	p_color=conf["hratio"][p]["color"]
+        myplotter.AddHist(p_path,p_name,p_title)
+	myplotter.SetLinecolor(p,p_color)               
+    myplotter.DrawRatio()
 
 if __name__ == '__main__':
     confpath=sys.argv[1]
