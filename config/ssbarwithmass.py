1710abdfe84042e443c02e55ed5c863f7068f29a
kkimeun <kkimeun22@snu.ac.kr>
2024-03-04 18:09:25 +0900

    invariant lepton mass -4

--- config/ssbarwithmass.py
+++ config/ssbarwithmass.py
@@ -3,16 +3,16 @@ from collections import OrderedDict
 _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
-    "name":"invariant lepton mass distribution using NNPDF31",
+    "name":"invariant mass of (lepton + neutrino) distribution using NNPDF31",
     "input":_maindir+"/mg_output/histos_invariantmass.root",
     "hist":OrderedDict({
-        "ll":{"path":"m_ll","name":"ll","title":"ll","color":6},
+        "lv":{"path":"m_lv","name":"lv","title":"lv","color":6},
         #"e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
         #"e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
     }),
-    "title":"invariant lepton mass distribution using NNPDF31_nnlo_hessian_pdfas",
-    "xtitle":"M(ll)",
+    "title":"invariant mass of (lepton + neutrino) distribution using NNPDF31_nnlo_hessian_pdfas",
+    "xtitle":"M(lv)",
     "ytitle":"Event",
-    "deno":"ll",
+    "deno":"lv",
 
 }
