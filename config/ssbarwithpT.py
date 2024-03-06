03ee92520f0956f3be3f1f482ae60011975ea563
kkimeun <kkimeun22@snu.ac.kr>
2024-03-06 17:46:26 +0900

    lepton pT < 5

--- config/ssbarwithpT.py
+++ config/ssbarwithpT.py
@@ -3,7 +3,7 @@ from collections import OrderedDict
 _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
-    "name":"electron pT using NNPDF31",
+    "name":"electron pT < 5 using NNPDF31",
     "input":_maindir+"/mg_output/histos_ssbarwithpTEta.root",
     "hist":OrderedDict({
         "e":{"path":"pT_e","name":"e","title":"e","color":1},
@@ -15,7 +15,7 @@ conf={
 	"e, x>0.0003":{"path":"pT_e_x>0.0003","name":"e, x>0.0003","title":"e, x>0.0003","color":2},
 	"e, x<0.0003":{"path":"pT_e_x<0.0003","name":"e, x<0.0003","title":"e, x<0.0003","color":4},
      }),
-    "title":"pp->l vl electron pT using NNPDF31_nnlo_hessian_pdfas",
+    "title":"pp->l vl electron pT < 5 using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"pT(e)",
     "ytitle":"Event",
     "deno":"e, x<0.0003",
