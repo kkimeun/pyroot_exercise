82edd34322fb13c14a8ffef91c9f0c6735806d47
kkimeun <kkimeun22@snu.ac.kr>
2024-03-06 15:25:19 +0900

    to change linecolor ratioplot -6

--- config/ssbarwithpT.py
+++ config/ssbarwithpT.py
@@ -3,8 +3,8 @@ from collections import OrderedDict
 _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
-    "name":"electron pT > 200 using NNPDF31",
-    "input":_maindir+"/mg_output/histos_ssbarwithpT.root",
+    "name":"electron pT using NNPDF31",
+    "input":_maindir+"/mg_output/histos_ssbarwithpTEta.root",
     "hist":OrderedDict({
         "e":{"path":"pT_e","name":"e","title":"e","color":6},
         "e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
@@ -15,7 +15,7 @@ conf={
 	"e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
 	"e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
      }),
-    "title":"pp->l vl electron pT > 200 using NNPDF31_nnlo_hessian_pdfas",
+    "title":"pp->l vl electron pT using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"pT(e)",
     "ytitle":"Event",
     "deno":"e, x<0.03",
