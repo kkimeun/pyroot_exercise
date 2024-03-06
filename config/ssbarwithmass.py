45f775fd4b46779873741d5b687eca27e57ba162
kkimeun <kkimeun22@snu.ac.kr>
2024-03-06 18:21:49 +0900

    change linecolor

--- config/ssbarwithmass.py
+++ config/ssbarwithmass.py
@@ -6,7 +6,7 @@ conf={
     "name":"invariant mass of (lepton + neutrino) distribution using NNPDF31",
     "input":_maindir+"/mg_output/histos_invariantmass.root",
     "hist":OrderedDict({
-        "lv":{"path":"m_lv","name":"lv","title":"lv","color":8},
+        "lv":{"path":"m_lv","name":"lv","title":"lv","color":3},
         #"e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
         #"e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
     }),
