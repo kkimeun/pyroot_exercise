55653543d043d2b963104b881119ee7577e519c7
kkimeun <kkimeun22@snu.ac.kr>
2024-03-06 17:54:29 +0900

    adjust pT histogram range 0 to 10, fix filepath s sbar config

--- config/ssbar_NNPDF31.py
+++ config/ssbar_NNPDF31.py
@@ -4,14 +4,14 @@ _maindir = os.getenv("_maindir_pyroot_git")
 
 conf={
     "name":"s_sbar with lepton pT < 5, using NNPDF31",
-    "input":_maindir+"/mg_output/histos_ssbarwithpT.root",
+    "input":_maindir+"/mg_output/histos_ssbarwithpTEta.root",
     #"input":_maindir+"/mg_output/histos_ssbarwithpT.root",
     "hist":OrderedDict({
         "s":{"path":"log_x_s","name":"s","title":"s","color":4},
         "sbar":{"path":"log_x_sbar","name":"sbar","title":"#bar{s}","color":2},
     }),
     "hratio":OrderedDict({
-	"sbar":{"path":"log_x_sbar","name":"sbar","title":"#bar{s}","color":2},
+	"sbar":{"path":"log_x_sbar","name":"sbar","title":"#bar{s}","color":1},
     }),
 
     "title":"pp->l vl with lepton pT < 5, using NNPDF31_nnlo_hessian_pdfas",
