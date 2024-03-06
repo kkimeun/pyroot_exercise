98602d7a87f70d87f0dde4409fed5c03413f029f
kkimeun <kkimeun22@snu.ac.kr>
2024-03-06 19:35:11 +0900

    mass < 30, to see mass distribution with x

--- config/ssbarwithmass.py
+++ config/ssbarwithmass.py
@@ -7,12 +7,17 @@ conf={
     "input":_maindir+"/mg_output/histos_invariantmass.root",
     "hist":OrderedDict({
         "lv":{"path":"m_lv","name":"lv","title":"lv","color":3},
-        #"e, x>0.03":{"path":"pT_e_x>0.03","name":"e, x>0.03","title":"e, x>0.03","color":2},
-        #"e, x<0.03":{"path":"pT_e_x<0.03","name":"e, x<0.03","title":"e, x<0.03","color":4},
+        "lv, x>0.0003":{"path":"m_lv_x>0.0003","name":"lv, x>0.0003","title":"lv, x>0.0003","color":2},
+        "lv, x<0.0003":{"path":"m_lv_x<0.0003","name":"lv, x<0.0003","title":"lv, x<0.0003","color":4},
     }),
+    "hratio":OrderedDict({
+        "lv":{"path":"m_lv","name":"lv","title":"lv","color":3},
+        "lv, x>0.0003":{"path":"m_lv_x>0.0003","name":"lv, x>0.0003","title":"lv, x>0.0003","color":2},
+        "lv, x<0.0003":{"path":"m_lv_x<0.0003","name":"lv, x<0.0003","title":"lv, x<0.0003","color":4},
+     }),
     "title":"invariant mass of (lepton + neutrino) distribution using NNPDF31_nnlo_hessian_pdfas",
     "xtitle":"M(lv)",
     "ytitle":"Event",
-    "deno":"lv",
+    "deno":"lv, x<0.0003",
 
 }
