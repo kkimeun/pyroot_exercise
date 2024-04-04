c816e76adc8efaa3eac56cb5a7d61f33c8b829af
kkimeun <kkimeun22@snu.ac.kr>
2024-04-04 14:01:57 +0900

    test to draw 2 graphs -3

--- test.py
+++ test.py
@@ -15,22 +15,37 @@ def GetStoSbar(x,q2,lhapdf_name):
     irep=0
     s_pdf=p.xfxQ2(pdgid,x,q2)
     sbar_pdf=p.xfxQ2(-pdgid,x,q2)
-    r_nom=s_pdf/sbar_pdf
-    sum_dr2=0
+    #r_nom=s_pdf/sbar_pdf
+    #sum_dr2=0
+    sum_ds2=0
+    sum_dsbar2=0
     for irep in range(Nrep):
         p = lhapdf.mkPDF(lhapdf_name,irep)
         
-        s_pdf=p.xfxQ2(pdgid,x,q2)
-        sbar_pdf=p.xfxQ2(-pdgid,x,q2)
+        #s_pdf=p.xfxQ2(pdgid,x,q2)
+        #sbar_pdf=p.xfxQ2(-pdgid,x,q2)
         #21, 1e-3, 1e4)
         #print s_pdf/sbar_pdf
-        r=s_pdf/sbar_pdf
-        dr=r-r_nom
-        dr2=dr*dr
-        sum_dr2+=dr2
-    r_err=sqrt(sum_dr2)
+        #r=s_pdf/sbar_pdf
+        #dr=r-r_nom
+        #dr2=dr*dr
+        #sum_dr2+=dr2
+
+	s=p.xfxQ2(pdgid,x,q2)
+        ds=s-s_pdf
+	ds2=ds*ds
+	sum_ds2+=ds2
+
+	sbar=p.xfxQ2(-pdgid,x,q2)
+	dsbar=sbar-sbar_pdf
+	dsbar2=dsbar*dsbar
+	sum_dsbar2+=dsbar2
+
+    #r_err=sqrt(sum_dr2)
+    s_err=sqrt(sum_ds2)
+    sbar_err=sqrt(sum_dsbar2)
     #return r_nom,r_err
-    return s_pdf,sbar_pdf
+    return s_pdf,sbar_pdf,s_err,sbar_err
 
 #def Draw(,):
     #c=ROOT.TCanvas()
@@ -65,21 +80,27 @@ lhapdf.setVerbosity(0)
 #for x in [0.3,0.25,0.2,0.1,0.06,0.05,0.04,0.03,0.02,0.01,0.008,0.005,0.003,0.001,0.0001,0.00001,1e-6,1e-7,1e-8,1e-9,1e-10,1e-11]:
 arr_logx=[]
 arr_x=[]
-arr_y=[]
-arr_z=[]
+arr_s=[]
+arr_serr=[]
+arr_sbar=[]
+arr_sbarerr=[]
 y=np.arange(-5,-1,0.5)
 for i in y:
     x= 10**i
-    s_pdf,sbar_pdf=GetStoSbar(x,Q2,lhapdf_name)
+    s_pdf,sbar_pdf,s_err,sbar_err=GetStoSbar(x,Q2,lhapdf_name)
     #r,r_err=GetStoSbar(x,Q2,lhapdf_name)
     #print x,'\t', s_pdf,"\t",r_err
     #print i,'\t',x,'\t', s_pdf, "\t",sbar_pdf
     arr_logx.append(i)
     arr_x.append(x)
-    arr_y.append(s_pdf)
-    arr_z.append(sbar_pdf)
+    arr_s.append(s_pdf)
+    arr_serr.append(s_err)
+    arr_sbar.append(sbar_pdf)
+    arr_sbarerr.append(sbar_err)
 print (arr_logx)
 print (arr_x)
-print (arr_y)
-print (arr_z)     
-np.savez('arrays.npz',arr_logx=arr_logx,arr_x=arr_x,arr_y=arr_y,arr_z=arr_z)
+print (arr_s)
+print (arr_serr)
+print (arr_sbar)
+print (arr_sbarerr)     
+np.savez('arrays.npz',arr_logx=arr_logx,arr_x=arr_x,arr_s=arr_s,arr_sbar=arr_sbar,arr_serr=arr_serr,arr_sbarerr=arr_sbarerr)
--- testarray.py
+++ testarray.py
@@ -6,36 +6,41 @@ c=ROOT.TCanvas()
 loaded_array=np.load('/home/snuintern3/LHAPDFexercise/LHAPDF_exercise/arrays.npz')
 arr_logx=loaded_array['arr_logx']
 #arr_x=loaded_array['arr_x']
-arr_y=loaded_array['arr_y']
-#arr_z=loaded_array['arr_z']
-gr=ROOT.TGraphErrors()
+arr_s=loaded_array['arr_s']
+arr_sbar=loaded_array['arr_sbar']
+arr_serr=loaded_array['arr_serr']
+arr_sbarerr=loaded_array['arr_sbarerr']
+
+gr1=ROOT.TGraphErrors()
 gr2=ROOT.TGraphErrors()
 mg=ROOT.TMultiGraph()
 N=len(arr_logx)
 for i in range(N):
     logx=arr_logx[i]
     #x=arr_x[i]
-    y=arr_y[i]
-    #z=arr_z[i]
+    s=arr_s[i]
+    sbar=arr_sbar[i]
     ex=0
-    ey=0
-    gr.SetPoint(i,logx,y)
-    gr.SetPointError(i,0,ey)
+    ey1=arr_serr[i]
+    ey2=arr_sbarerr[i]
+    gr1.SetPoint(i,logx,s)
+    gr1.SetPointError(i,0,ey1)
     
-    gr2.SetPoint(i,-3,2)
-    gr2.SetPointError(i,0,1)
+    gr2.SetPoint(i,logx,sbar)
+    gr2.SetPointError(i,0,ey2)
+
 gr2.SetMarkerColor(2)
 gr2.SetMarkerSize(0.3)
 gr2.SetMarkerStyle(20)
 #gr2.Draw("ap4")
 #mycolor=4
 #gr.SetFillColorAlpha(mycolor,0.3)
-gr.SetMarkerColor(4)
-gr.SetMarkerSize(0.3)
-gr.SetMarkerStyle(20)
+gr1.SetMarkerColor(4)
+gr1.SetMarkerSize(0.3)
+gr1.SetMarkerStyle(20)
 #gr.Draw("ap3")
 
-mg.Add(gr)
+mg.Add(gr1)
 mg.Add(gr2)
 mg.Draw("ap3")
 mg.SetTitle("Test")
