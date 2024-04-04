cae27fbba12b56c26665265e4553342f1316d2f4
kkimeun <kkimeun22@snu.ac.kr>
2024-04-04 14:09:45 +0900

    test to draw 2 graphs -6

--- testarray.py
+++ testarray.py
@@ -12,38 +12,38 @@ arr_serr=loaded_array['arr_serr']
 arr_sbarerr=loaded_array['arr_sbarerr']
 
 gr1=ROOT.TGraphErrors()
-gr2=ROOT.TGraphErrors()
-mg=ROOT.TMultiGraph()
+#gr2=ROOT.TGraphErrors()
+#mg=ROOT.TMultiGraph()
 N=len(arr_logx)
 for i in range(N):
     logx=arr_logx[i]
     #x=arr_x[i]
     s=arr_s[i]
-    sbar=arr_sbar[i]
+    #sbar=arr_sbar[i]
     ex=0
     ey1=arr_serr[i]
-    ey2=arr_sbarerr[i]
+    #ey2=arr_sbarerr[i]
     gr1.SetPoint(i,logx,s)
     gr1.SetPointError(i,0,1)
     
-    gr2.SetPoint(i,logx,sbar)
-    gr2.SetPointError(i,0,ey2)
+    #gr2.SetPoint(i,logx,sbar)
+    #gr2.SetPointError(i,0,ey2)
 
-gr2.SetMarkerColor(2)
-gr2.SetMarkerSize(0.5)
-gr2.SetMarkerStyle(20)
+#gr2.SetMarkerColor(2)
+#gr2.SetMarkerSize(0.5)
+#gr2.SetMarkerStyle(20)
 #gr2.Draw("ap4")
 #mycolor=4
 #gr.SetFillColorAlpha(mycolor,0.3)
 gr1.SetMarkerColor(4)
 gr1.SetMarkerSize(0.5)
 gr1.SetMarkerStyle(20)
-#gr.Draw("ap3")
+gr1.Draw("ap3")
 
-mg.Add(gr1)
-mg.Add(gr2)
-mg.Draw("ap3")
-mg.SetTitle("Test")
+#mg.Add(gr1)
+#mg.Add(gr2)
+#mg.Draw("ap3")
+#mg.SetTitle("Test")
 c.SaveAs("test.pdf")
 
 #print x
